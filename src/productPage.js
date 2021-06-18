b123496feee5a53f03dab90d1510ad8ce87693b7
Uto <daffamadeo@gmail.com>
2021-06-18 19:26:43 +0700

    a bunch of shit

--- src/productPage.js
+++ src/productPage.js
@@ -1,8 +1,7 @@
 function openSlideMenu(){
     document.getElementById('menu').style.width = '250px';
-    document.getElementById('content').style.marginLeft = '250px';
-  }
-  function closeSlideMenu(){
+}
+
+function closeSlideMenu(){
     document.getElementById('menu').style.width = '0';
-    document.getElementById('content').style.marginLeft = '0';
   }
